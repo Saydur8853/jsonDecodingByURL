6907df7e74a8b43048be9d08c5de9ff38735a410
SAYDUR <saydur@labs-mac.local>
2020-01-22 17:34:25 +0600

    jsonDecodingByURLsession

--- jsonDecoding/ViewController.swift
+++ jsonDecoding/ViewController.swift
@@ -8,11 +8,36 @@
 
 import UIKit
 
+struct Country: Decodable {
+    let name: String
+    let capital: String
+    let region: String
+}
+
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        let url = "https://restcountries.eu/rest/v2/all"
+        let urlObj = URL(string: url)
+        
+        URLSession.shared.dataTask(with: urlObj!) {(data, response, error) in
+            
+            do{
+                let countries = try JSONDecoder().decode([Country].self, from: data!)
+                
+                for Country in countries {
+                    print(Country.name + ":" + Country.capital)
+                }
+                
+            } catch{
+                //return()//print("Not me error")
+            }
+            }.resume()
     }
 
 
